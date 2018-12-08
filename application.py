2340622107897f0f82b2d1cd33916a3d2b45fffc
robert <robert.lizatovic@gmail.com>
2018-12-08 19:42:55 +0100

    added async data fetching from server

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
 import pandas as pd
 import numpy as np
+import json
 
 app = Flask(__name__)
 
@@ -19,6 +20,15 @@ def plot():
     y = np.sin(x) + np.random.normal(size=100)
     script_src = url_for("static", filename="plot.js")
     return render_template("plot.html", x=x, y=y, script_src=script_src)
+
+@app.route("/data")
+def fecth_data():
+    x = list(np.linspace(1, 100, 100))
+    y = list(np.sin(x) + np.random.normal(size=100))
+    data = {"x": x, "y": y}
+    # serve the plotting data as json
+    return json.dumps(data)
+
 
 if __name__ == "__main__":
     app.run()
--- static/plot.js
+++ static/plot.js
@@ -1,6 +1,15 @@
 var div = document.getElementById("plot");
 
-var data = {x: [1, 2, 3],
-            y: [4, 5, 6]}
+var data_test = {x: [1, 2, 3], y: [4, 5, 6]};
 
-Plotly.plot(div, [data], {margin: {t:0}})
+// an empty object holding the data to plot
+var plot_data = {};
+
+// get data from python via AJAX/JQuery
+$.getJSON("data", function (data) {
+    console.log("data obtained!");
+    $.each(data, function (k,v) {
+        plot_data[k] = v;
+    })
+    Plotly.plot(div, [plot_data], {margin: {t:0}})
+})
